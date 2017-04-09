83e87a268371460762f31b09f9dcf5ef4144a3c3
hyunji9340 <hjn9340@gmail.com>
2017-04-09 12:26:07 -0700

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System;
+
+using Xamarin.Forms;
+
+namespace XamarinProject
+{
+	public partial class App : Application
+	{
+		public static bool UseMockDataStore = true;
+		public static string BackendUrl = "https://localhost:5000";
+
+		public static IDictionary<string, string> LoginParameters => null;
+
+		public App()
+		{
+			InitializeComponent();
+
+			if (UseMockDataStore)
+				DependencyService.Register<MockDataStore>();
+			else
+				DependencyService.Register<CloudDataStore>();
+
+			SetMainPage();
+		}
+
+		public static void SetMainPage()
+		{
+			if (!UseMockDataStore && !Settings.IsLoggedIn)
+			{
+				Current.MainPage = new NavigationPage(new LoginPage())
+				{
+					BarBackgroundColor = (Color)Current.Resources["Primary"],
+					BarTextColor = Color.White
+				};
+			}
+			else
+			{
+				GoToMainPage();
+			}
+		}
+
+		public static void GoToMainPage()
+		{
+			Console.WriteLine("before calling new info class");
+			Current.MainPage = new TabbedPage
+			{
+				Children = {
+					new NavigationPage(new MyInfo()) // new ItemPage()
+					{
+						Title = "Browse" // Browse
+					},
+					new NavigationPage(new AboutPage())
+					{
+						Title = "About" // About
+					},
+				}
+			};
+		}
+	}
+}
