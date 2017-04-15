a7fca2be4db658a5513419dfec782cf69e667c3a
hyunji9340 <hjn9340@gmail.com>
2017-04-15 16:17:44 -0700

    Update MonsterPage.xaml.cs

--- Views/MonsterPage.xaml.cs
+++ Views/MonsterPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 using Xamarin.Forms;
@@ -7,9 +7,48 @@ namespace XamarinProject
 {
 	public partial class MonsterPage : ContentPage
 	{
+		private List<Monster> monsters;
+
 		public MonsterPage()
 		{
 			InitializeComponent();
+			PopulateMonsters();
+			MonsterListView.ItemsSource = monsters;
 		}
+
+		public void PopulateMonsters() 
+		{
+			monsters = new List<Monster>();
+			Monster monster1 = new Monster();
+			Monster monster2 = new Monster();
+			Monster monster3 = new Monster();
+			Monster monster4 = new Monster();
+			Monster monster5 = new Monster();
+			monster1.name = "monster1";
+			monster2.name = "mosnter2";
+			monster3.name = "monster3";
+			monster4.name = "monster4";
+			monster5.name = "monster5";
+			monsters.Add(monster1);
+			monsters.Add(monster2);
+			monsters.Add(monster3);
+			monsters.Add(monster4);
+			monsters.Add(monster5);
+		}
+
+
+		async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+		{
+			var monster = args.SelectedItem as Monster;
+
+			if (monster == null)
+				return;
+
+			//await Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(monster)));
+
+			// Manually deselect item
+			MonsterListView.SelectedItem = null;
+		}
+
 	}
 }
