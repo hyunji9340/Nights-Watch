b4d464a964e8f629185c32c544512ccf9fb7d8c5
Zach <Zach@ZACH-PC>
2017-04-29 11:08:04 -0700

    Build Successful: added battle sounds to battle, randomly switches between 4 different sounds when selecting the step button.

--- Views/BattlePage.xaml.cs
+++ Views/BattlePage.xaml.cs
@@ -10,17 +10,39 @@ namespace GroupProject_DD
 	{
 		private BattleViewModel BattleEngineView;
 		private int counter;
-
+        Random rand;
 		public BattlePage()
 		{
 			InitializeComponent();
 			BindingContext = BattleEngineView = new BattleViewModel();
 			counter = 0;
+            rand = new Random();
+            
 		}
 
 		async void Button_Clicked(object sender, EventArgs e)
 		{
-			BattleEngineView.UpdateAction(ref counter);
+            string battleSound = ".mp3";
+            switch (rand.Next(1, 5))
+            {
+                case 1:
+                    battleSound = "battleA" + battleSound;
+                    break;
+                case 2:
+                    battleSound = "battleB" + battleSound;
+                    break;
+                case 3:
+                    battleSound = "battleC" + battleSound;
+                    break;
+                case 4:
+                    battleSound = "battleD" + battleSound;
+                    break;
+                default:
+                    battleSound = "battleA" + battleSound;
+                    break;
+            }
+            DependencyService.Get<IAudioPlayerService>().Play(battleSound);
+            BattleEngineView.UpdateAction(ref counter);
 		}
 	}
 }
