e69744d41d730acfa96040750e115ba0683307dd
hyunji9340 <hjn9340@gmail.com>
2017-05-08 22:04:49 -0700

    Update ScorePage.xaml.cs

--- Views/ScorePage.xaml.cs
+++ Views/ScorePage.xaml.cs
@@ -1,15 +1,30 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 using Xamarin.Forms;
 
-namespace XamarinProject
+namespace GroupProject_DD
 {
 	public partial class ScorePage : ContentPage
 	{
+		private PlayerController playerController;
+
 		public ScorePage()
 		{
 			InitializeComponent();
+			this.playerController = new PlayerController();
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+			ScoreListView.ItemsSource = this.playerController.OrderedPlayers();
+		}
+
+		// directs to monster page
+		async void newGameBtnClicked(object sender, EventArgs e)
+		{
+			await Navigation.PopToRootAsync();
 		}
 	}
 }
