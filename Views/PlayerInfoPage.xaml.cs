3598721e3c9e0dcb337441c475a855eb1b8be717
hyunji9340 <hjn9340@gmail.com>
2017-05-08 22:03:31 -0700

    Add files via upload

---
+++ Views/PlayerInfoPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using Xamarin.Forms;
+
+namespace GroupProject_DD
+{
+	public partial class PlayerInfoPage : ContentPage
+	{
+		private PlayerController playerController;
+
+		public PlayerInfoPage()
+		{
+			InitializeComponent();
+			this.playerController = new PlayerController();
+		}
+
+		async void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			// create a player instance and store in db
+			Player newPlayer = new Player();
+			newPlayer.Name = playerName.Text;
+			playerController.SavePlayer(newPlayer);
+			await Navigation.PushAsync(new GameMainPage(newPlayer));
+		}
+
+	}
+}
